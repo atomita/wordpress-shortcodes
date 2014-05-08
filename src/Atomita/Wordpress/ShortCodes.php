612b8a86c25946eca7516cfb8a112f66aa571f71
@ <sleeping.cait.sith+gh@gmail.com>
2014-05-08 14:45:48 +0900

    Update ShortCodes.php

--- src/Atomita/Wordpress/ShortCodes.php
+++ src/Atomita/Wordpress/ShortCodes.php
@@ -83,4 +83,56 @@ class ShortCodes
 		return '';
 	}
 
+
+	public function title($atts)
+	{
+		$attrs = shortcode_atts(array('', '', 'slug' => ''), $atts);
+		$slug  = empty($attrs[0]) ? (empty($attrs['slug']) ? '' : $attrs['slug']) : $attrs[0];
+
+		global $wpdb;
+		$query = $wpdb->prepare("select ID from {$wpdb->posts} where post_name = %s", $slug);
+		$pages = $wpdb->get_results($query);
+		if (empty($pages)){
+			return '';
+		}
+		return get_the_title($pages[0]->ID);
+	}
+
+	public function excerpt($atts)
+	{
+		$attrs = shortcode_atts(array('', 'slug' => ''), $atts);
+		$slug  = empty($attrs[0]) ? (empty($attrs['slug']) ? '' : $attrs['slug']) : $attrs[0];
+
+		global $wpdb;
+		$query = $wpdb->prepare("select post_excerpt from {$wpdb->posts} where post_name = %s", $slug);
+		$pages = $wpdb->get_results($query);
+		if (empty($pages)){
+			return '';
+		}
+		return apply_filters('the_excerpt', apply_filters('get_the_excerpt', $pages[0]->post_excerpt));
+	}
+
+	public function content($atts)
+	{
+		$attrs = shortcode_atts(array('', null, false, 'slug' => '', 'more' => null, 'strip_teaser' => false), $atts);
+		$slug  = empty($attrs[0]) ? (empty($attrs['slug']) ? '' : $attrs['slug']) : $attrs[0];
+		$more  = empty($attrs[1]) ? (empty($attrs['more']) ? '' : $attrs['more']) : $attrs[1];
+		$strip = empty($attrs[2]) ? (empty($attrs['strip_teaser']) ? '' : $attrs['strip_teaser']) : $attrs[2];
+
+		global $wpdb;
+		$query = $wpdb->prepare("select ID from {$wpdb->posts} where post_name = %s", $slug);
+		$pages = $wpdb->get_results($query);
+		if (empty($pages)){
+			return '';
+		}
+
+		global $post;
+		$_post	 = $post;
+		$post	 = get_post($pages[0]->ID);
+		setup_postdata($post);
+		$content = get_the_content($more, $strip);
+		$post	 = $_post;
+		setup_postdata($post);
+		return $content;
+	}
 }
